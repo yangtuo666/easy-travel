784689fe0ff89077b6a578d211b5d793370c1138
九章(张宁宁) <ningning.zhang@yaduo.com>
2026-05-09 15:21:57 +0800

    项目初始化

---
+++ src/main/java/com/znn/easytravel/EasyTravelApplication.java
@@ -0,0 +1,13 @@
+package com.znn.easytravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyTravelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/znn/easytravel/EasyTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.znn.easytravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyTravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
